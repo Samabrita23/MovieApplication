47f6f2bf981ffb5ecfc01819e4c291ae225965a2
Samabrita <samabritabiswas2302@gmail.com>
2023-06-11 14:39:33 +0530

    Page Header created

---
+++ src/Components/PageHeader.tsx
@@ -0,0 +1,30 @@
+// PageHeader.tsx
+import React from 'react';
+import '../Styles/PageHeader.css';
+import logo from '../Assets/logo.svg';
+
+ const PageHeader = () => {
+  return (
+    <header className="page-header">
+      <div className="logo">
+        <svg width="100" height="50" viewBox="0 0 100 50">
+          <image href={logo} width="100" height="50" />
+        </svg>
+      </div>
+      <nav className="navigation">
+        <ul>
+          <li><a href="#">Movies</a></li>
+          <li><a href="#">TV Shows</a></li>
+          <li><a href="#">People</a></li>
+          <li><a href="#">More</a></li>
+        </ul>
+      </nav>
+      <div className="user-actions">
+        <a href="#">Login</a>
+        <a href="#">Join Us</a>
+        {/* <button><i className="fa fa-search"></i></button> */}
+      </div>
+    </header>
+  );
+};
+ export default PageHeader;
